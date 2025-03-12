396020fc76500b209e6c32df388f27b7a11e5c60
bbqcwz <yangyt77sh@berkeley.edu>
2025-03-11 17:27:05 -0700

    Updates

---
+++ precache-manifest.3a325c58d879ab177c1131d4acb58c1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9c42289e19def5e22ff18b026680d9",
+    "url": "/startup-landing-page/index.html"
+  },
+  {
+    "revision": "6c3e0dfcc5647fa03ead",
+    "url": "/startup-landing-page/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5180cec8b664a6906ea0",
+    "url": "/startup-landing-page/static/js/2.f2401a20.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/startup-landing-page/static/js/2.f2401a20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f4565eb91ba254d57b",
+    "url": "/startup-landing-page/static/js/3.db3b59c1.chunk.js"
+  },
+  {
+    "revision": "6c3e0dfcc5647fa03ead",
+    "url": "/startup-landing-page/static/js/main.3fbdc440.chunk.js"
+  },
+  {
+    "revision": "22c90e6414504aee9ac4",
+    "url": "/startup-landing-page/static/js/runtime-main.a8f89d0c.js"
+  }
+]);
